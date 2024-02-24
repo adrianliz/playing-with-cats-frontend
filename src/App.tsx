1a9a38ed6e2dd983713084e6fda3906e6c30f6e0
Adrián Lizaga <lizagaadrian@gmail.com>
2024-02-24 17:15:59 +0100

    refactor: use question hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
-import {CatBreed, Question} from "./models/Question.tsx";
 import QuestionCard from "./components/QuestionCard.tsx";
-import {useEffect, useState} from "react";
-
-export const API_URL = 'http://localhost:8080/questions'
+import useQuestion from "./hooks/useQuestion.tsx";
 
 export default function App() {
-    const [question, setQuestion] = useState<Question>()
-
-    useEffect(() => {
-        fetch(API_URL, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json'
-            }
-        })
-            .then(response => response.json())
-            .then(data => {
-                setQuestion({
-                    id: data.id,
-                    breeds: data.breeds.map((breed: CatBreed) => {
-                        return {
-                            id: breed.id,
-                            name: breed.name
-                        }
-                    }),
-                    cat: {
-                        id: data.cat.id,
-                        imageUrl: data.cat.imageUrl
-                    }
-                })
-            })
-    }, [])
+    const {question, loading} = useQuestion()
 
     return (
-        <div className='container flex flex-col max-w-sm max-h m-auto h-screen'>
-            {question && <QuestionCard question={question}/> }
+        <div className="container flex flex-col max-w-sm max-h m-auto h-screen">
+            {loading && <h1 className="m-auto text-center">Loading...</h1>}
+            {question && <QuestionCard question={question} />}
         </div>
     )
 }
